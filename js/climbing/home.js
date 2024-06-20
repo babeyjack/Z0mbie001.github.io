e1e55b318d9885b337f596d04786948f18f4470d
Z0mbie001 <94248812+Z0mbie001@users.noreply.github.com>
2024-06-20 19:29:18 +0100

    Added a slidshow

--- js/climbing/home.js
+++ js/climbing/home.js
@@ -1,11 +1,26 @@
-// For the hamburger menu
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+var slideIndex = 1;
+showDivs(slideIndex);
 
-hamburger.addEventListener("click", mobileMenu);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
 
-function mobileMenu()
-{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
 }
