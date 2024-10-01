754e6c62fb4973a2dc0dbd4ce492484c2f6e2ed4
Jack Babey <jackbabey@gmail.com>
2024-10-01 15:57:16 +0000

    Fixed Dropdowns

--- js/profile.js
+++ js/profile.js
@@ -20,6 +20,7 @@ function refreshQualifications()
                 this.classList.toggle("active");
                 var content = this.nextElementSibling;
                 if (content.style.display === "block") {
+                    content.style.display = "none";
                 } else {
                     content.style.display = "block";
 
