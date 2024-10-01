be575e587e290b520588199c83dcbfa114ff5874
Jack Babey <jackbabey@gmail.com>
2024-10-01 15:43:46 +0000

    Made opening stuff open

--- js/profile.js
+++ js/profile.js
@@ -20,9 +20,9 @@ function refreshQualifications()
                 this.classList.toggle("active");
                 var content = this.nextElementSibling;
                 if (content.style.display === "block") {
-                    content.style.display = "none";
                 } else {
                     content.style.display = "block";
+
                 }
             });
 
